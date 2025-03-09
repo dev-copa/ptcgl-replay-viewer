3335ea00a4cab8055169d650ccb8a1ae9a3deee8
dev-copa <shaman.hsdecks@gmail.com>
2025-03-09 22:22:10 +0100

    change of localhost url

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.setViewEngine('ejs');
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 8000;
   await app.listen(port);
-  console.log(`Application is running on: ${port}`);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
